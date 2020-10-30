6e170838672f30e21d44f905e406891e8c541dbf
Glitch (imh-host) <none>
2020-10-30 22:40:33 +0000

    Add stats

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
-const express = require("express");
-const fs = require("fs");
-const multer = require("multer");
+const express = require('express');
+const fs = require('fs');
+const multer = require('multer');
 const upload = multer();
 const app = express();
-const imgur = require("imgur");
+const imgur = require('imgur');
+const jsonfile = require('jsonfile');
+
+app.enable('trust proxy');
+app.use(function(request, response, next) {
+  if (process.env.NODE_ENV != 'development' && !request.secure) {
+    response.redirect("https://" + request.headers.host + request.url) 
+  } 
+  next();
+});
 
 imgur.setClientId(process.env.CLIENT_ID);
 
 app.get('/', (req, res) => {
   res.send("IMH UP");
+});
+
+app.get('/stats', (req, res) => {
+  let stats = jsonfile.readFileSync("stats.json");
+  res.send(stats.uploads.toString());
 });
 
 app.get('/*', (req, res) => {
@@ -25,6 +39,10 @@ app.post("/upload", upload.any(), async (req, res) => {
       url = "i/" + url;
     };
       
+    let stats = jsonfile.readFileSync("stats.json");
+    stats["uploads"]++;
+    jsonfile.writeFileSync("stats.json", stats);
+    
     return res.status(200).json({ data: { link: url } });
   }).catch(function(err) {
     console.error(err.message);
